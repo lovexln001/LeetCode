2650e271b3dc7285fd54820a0a3e6ede439ebc9d
zhaobinbin <lovexln001@163.com>
2018-04-19 01:35:48 -0700

    add 120.Triangle

---
+++ 120.Triangle/solution.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        n = len(triangle)
+
+        dp = triangle[n-1]
+        
+        for i in xrange(n-2, -1 ,-1):
+            for j in xrange(i+1):
+                dp[j] = min(dp[j], dp[j+1]) +triangle[i][j]
+                
+        return dp[0]
